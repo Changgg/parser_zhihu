f2668706205e264275d8051b748f5eb8bdb59d53
Chang <HeawerCher@gmail.com>
2017-06-01 07:10:38 +0800

    基础更改 README 新增内容

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Passer-zhihu
 用Python看知乎
+
+## 修改内容
+- [x] 全部代码遵守基本规范
+- [x] mac和linux下从chrome获取cookies并验证
+- [ ] User添加写cookies值到文件的方法
+- [ ] User在init的时候加入路径判断，对路径下以用户id为首的文件进行cookies读取和判断，
+- [ ] 多用户模式
+
+
--- Utils.py
+++ Utils.py
@@ -1,14 +1,19 @@
 import time
+import os
+import faker
 
-Default_Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'Host': 'www.zhihu.com'}
+Default_Headers = {
+    'User-Agent': faker.Faker().chrome(),
+    'Host': 'www.zhihu.com'
+}
 COOKIE = ''
+PATH_FILES = os.path.join(os.path.dirname(__file__),'files')
 
 BASE_URL = 'https://www.zhihu.com'
 ZHUANLAN_URL = 'https://zhuanlan.zhihu.com'
 
 PHONE_LOGIN = BASE_URL + '/login/phone_num'
-CAPTCHA_URL = BASE_URL+'/captcha.gif?r='+str(int(time.time())*1000)+'&type=login'
+CAPTCHA_URL = BASE_URL + '/captcha.gif?r=' + str(int(time.time()) * 1000) + '&type=login'
 
 QUESTION_API = BASE_URL + '/api/v4/questions'
 ANSWER_API = BASE_URL + '/api/v4/answers'
