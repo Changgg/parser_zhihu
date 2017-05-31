b77ac6fd98abb37b35246db0e183697bd34153ce
Chang <HeawerCher@gmail.com>
2017-06-01 07:42:24 +0800

    UserAgent 使用随机值 整理 Utils 命名

--- Utils.py
+++ Utils.py
@@ -1,28 +1,34 @@
 import time
 import os
 import faker
+import requests
 
-Default_Headers = {
-    'User-Agent': faker.Faker().chrome(),
-    'Host': 'www.zhihu.com'
-}
-COOKIE = ''
-PATH_FILES = os.path.join(os.path.dirname(__file__),'files')
+BROWSER_HEADERS_DEFAULT = requests.structures.CaseInsensitiveDict(
+    {
+        'User-Agent': faker.Faker().chrome(),
+        'Accept-Encoding': ', '.join(('gzip', 'deflate')),
+        'Accept': '*/*',
+        'Connection': 'keep-alive',
+        'Host': 'www.zhihu.com'
+    }
+)
 
-BASE_URL = 'https://www.zhihu.com'
-ZHUANLAN_URL = 'https://zhuanlan.zhihu.com'
+PATH_FILES = os.path.join(os.path.dirname(__file__), 'files')
 
-PHONE_LOGIN = BASE_URL + '/login/phone_num'
-CAPTCHA_URL = BASE_URL + '/captcha.gif?r=' + str(int(time.time()) * 1000) + '&type=login'
+URL_BASE = 'https://www.zhihu.com'
+URL_ZHUANLAN = 'https://zhuanlan.zhihu.com'
 
-QUESTION_API = BASE_URL + '/api/v4/questions'
-ANSWER_API = BASE_URL + '/api/v4/answers'
+URL_PHONE_LOGIN = f'{URL_BASE}/login/phone_num'
+URL_CAPTCHA = f'{URL_BASE}/captcha.gif?r={str(int(time.time()) * 1000)}&type=login'
 
-PERSOINFO_API = BASE_URL + '/people/'
-MEMBER_API = BASE_URL + '/api/v4/members'
+URL_QUESTION_API = f'{URL_BASE}/api/v4/questions'
+URL_ANSWER_API = f'{URL_BASE}/api/v4/answers'
 
-TOPIC_API = BASE_URL + '/topic/'
+URL_PERSOINFO_API = f'{URL_BASE}/people/'
+URL_MEMBER_API = f'{URL_BASE}/api/v4/members'
 
-ZHUANLAN_ARTICLE = ZHUANLAN_URL + '/api/columns'
+URL_TOPIC_API = f'{URL_BASE}/topic/'
+
+URL_ZHUANLAN_ARTICLE = f'{URL_ZHUANLAN}/api/columns'
 
 ENG_FLAG = 'End'
